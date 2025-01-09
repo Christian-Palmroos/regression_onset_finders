65204b7a417c8a908fedd972722bc5df154ac7e8
Christian-Palmroos <chospa@utu.fi>
2025-01-09 15:39:23 +0200

    Added the function generate_fit_lines() to visualize the two lopes on a plot.

--- reg/calc_utilities.py
+++ reg/calc_utilities.py
@@ -11,6 +11,11 @@ import pandas as pd
 def resample_df(df, avg):
     """
     Resamples a dataframe such that care is taken on the offset and origin of the data index.
+
+    Parameters:
+    ----------
+    df : {pd.DataFrame}
+    avg : {str} Resampling string.
     """
 
     if isinstance(avg,str):
@@ -22,6 +27,48 @@ def resample_df(df, avg):
     copy_df.index = copy_df.index + pd.tseries.frequencies.to_offset(avg/2)
 
     return copy_df
+
+
+def generate_fit_lines(indices, const, alpha1, alpha2, breakpoint):
+    """
+    Generates two lines from fit parameters.
+    
+    Parameters:
+    ----------
+    indices : {array-like} The numerical indices of the data, the x-axis.
+    const : {float} The constant of the first linear fit.
+    alpha1 : {float} The slope of the first linear fit.
+    alpha2 : {float} The slope of the second linear fit.
+    breakpoint : {float} The point at which the gradient changes from alpha1 to alpha2.
+
+    Returns:
+    --------
+    line1 : {pd.Series} the first line until breakpoint.
+    line2 : {pd.Series} the second line from breakpoint to first peak.
+    """
+
+    # Working with indices, the breaking point of the two lines must
+    # be an integer
+    bp_int = int(breakpoint)
+
+    # For the first line just take indices from start to the breakpoint
+    indices_sel1 = indices[:bp_int]
+
+    # For the second part we need two sets of indices; one running from 0 -> len(indices2)
+    # to calculate the values, and the latter part of indices itself to index the values.
+    indices_sel2 = indices[bp_int:]
+    num_indices2 = len(indices_sel2)
+
+    # The first line is generated very simply from start to breakpoint
+    line1 = indices_sel1 * alpha1 + const
+
+    # Depending on the orientation of the first line, line2 starts from the max or the min of the line1
+    if alpha1 > 0:
+        line2 = np.linspace(start=0, stop=num_indices2, num=num_indices2) * alpha2 + np.nanmax(line1)
+    else:
+        line2 = np.linspace(start=0, stop=num_indices2, num=num_indices2) * alpha2 + np.nanmin(line1)
+
+    return pd.Series(line1, index=indices_sel1), pd.Series(line2, index=indices_sel2)
 
 
 def search_first_peak(ints, window=None, threshold=None):
